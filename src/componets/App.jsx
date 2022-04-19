bfae0d97b4fa4f23cf974bcee5e8618adb850bdf
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-04-19 07:57:37 -0700

    react and server configuration

--- src/componets/App.jsx
+++ src/componets/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const App = () => {
-    return(
-        <h1>Hello World</h1>
+    return (
+    <h1>Hello World! Welcome to Himalaya</h1>
     );
 }
 export default App;
